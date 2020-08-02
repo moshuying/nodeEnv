5d5ff8544add8223c81a02f9f24bf99eca1c1eff
moshuying <moshuyingnode@gmail.com>
2020-08-02 08:04:14 +0800

    双地图

--- recat-mapbox/src/pages/baseMap/index.js
+++ recat-mapbox/src/pages/baseMap/index.js
@@ -1,20 +1,16 @@
 import React, { Component } from "react";
 import mapboxGl from "mapbox-gl";
-import "./baseMap.scss"
+import "./baseMap.scss";
 
-mapboxGl.accessToken = "pk.eyJ1IjoibW9zaHV5aW5nIiwiYSI6ImNrYzJ3d3p1MjA0N3kyd3RnZXFpejlrZ3UifQ.8BVZUdzrBZqO7i5tVqbiUw";
+mapboxGl.accessToken =
+  "pk.eyJ1IjoibW9zaHV5aW5nIiwiYSI6ImNrYzJ3d3p1MjA0N3kyd3RnZXFpejlrZ3UifQ.8BVZUdzrBZqO7i5tVqbiUw";
 export default class home extends Component {
   map1 = null;
   map2 = null;
-  map1dom = <div id="map1"></div>
-  map2dom = <div id="map2"></div>
-  showMap = ''
-  dom = <div id="base-map" className={this.showMap}>{this.map1dom}
-  <div className="three-canvas">
-  <canvas id="world_canvas" style={{width: '100%', height: '100%'}}></canvas>
-  <canvas id="deckgl-overlay" style={{width: '100%', height: '100%'}}></canvas>
-</div>
-  {this.map2dom}</div>
+  map1dom = (<div id="map1"></div>);
+  map2dom = (<div id="map2"></div>);
+  showMap = "";
+  center  = [120.74861755955942, 31.160702143438556]
   constructor(props) {
     super(props);
     this.state = {};
@@ -22,34 +18,56 @@ export default class home extends Component {
   componentDidMount() {
     this.map1 = new mapboxGl.Map({
       container: "map1",
-      style: "mapbox://styles/mapbox/light-v10", // stylesheet location
-      center: [-74.5, 40], // starting position [lng, lat]
-      zoom: 9, // starting zoom
+      style: "mapbox://styles/mapbox/dark-v10", // stylesheet location
+      center:this.center, // starting position [lng, lat]
+      zoom: 1, // starting zoom
     });
     this.map2 = new mapboxGl.Map({
       container: "map2",
-      style: "mapbox://styles/mapbox/dark-v10", // stylesheet location
-      center: [-74.5, 40], // starting position [lng, lat]
-      zoom: 9, // starting zoom
+      style: "mapbox://styles/mapbox/light-v10", // stylesheet location
+      center:this.center, // starting position [lng, lat]
+      zoom: 1, // starting zoom
       doubleClickZoom: false,
       trackResize: false, //禁止地图响应浏览器屏幕大小缩放
     });
-    this.map1.on('load',()=>{
-      this.addlin()
-    })
+    this.map1.on("load", () => {
+      this.mapListener();
+    });
   }
-  addlin() {
-    this.showMap = 'index-mapbox showmap'
-    this.map1.on('move', () => {
+  mapListener() {
+    this.showMap = "index-mapbox showmap";
+    this.map1.on("move", () => {
       this.map2.jumpTo({
-        center:this.map1.getCenter(),
-        bearing:this.map1.getBearing(),
-        pitch:this.map1.getPitch(),
+        center: this.map1.getCenter(),
+        bearing: this.map1.getBearing(),
+        pitch: this.map1.getPitch(),
         zoom: this.map1.getZoom(),
-      })
+      });
+    });
+    this.map1.on('click',(e) => {
+      console.log(
+        `center:${JSON.stringify(
+          e.target.getCenter().toArray()
+        )},\nbearing:${e.target.getBearing()},\npitch:${e.target.getPitch()},\nzoom:${e.target.getZoom()}`
+      );
     })
   }
   render() {
-    return this.dom
+    return (
+      <div id="base-map" className={this.showMap}>
+        {this.map1dom}
+        <div className="three-canvas">
+          <canvas
+            id="world_canvas"
+            style={{ width: "100%", height: "100%" }}
+          ></canvas>
+          <canvas
+            id="deckgl-overlay"
+            style={{ width: "100%", height: "100%" }}
+          ></canvas>
+          {this.map2dom}
+        </div>
+      </div>
+    );
   }
 }
