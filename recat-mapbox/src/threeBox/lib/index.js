29229d477c75a9098ccddbcb4616bf2a9cddef6f
moshuying <moshuyingnode@gmail.com>
2020-08-06 14:42:06 +0800

    未能成功复现bug

--- recat-mapbox/src/threeBox/lib/index.js
+++ recat-mapbox/src/threeBox/lib/index.js
@@ -2,7 +2,7 @@ import ThreeBox from "../ThreeBox"
 import Scan from "./effectStore/Scan"
 import Shine from "./effectStore/Shine"
 import Physics from "./effectStore/Physics"
-import FixBug from "./effectStore/FixBug"
+
 class THREE extends ThreeBox{
   constructor(map,size,threeDomId,center,react){
     super(map,size,threeDomId,center)
@@ -11,9 +11,8 @@ class THREE extends ThreeBox{
     this.init()
   }
   loaded(){
-    // this.register(new Shine(this))
-    // this.register(new Physics(this))
-    this.register(new FixBug(this))
+    this.register(new Shine(this))
+    this.register(new Physics(this))
   }
 }
 export default THREE
